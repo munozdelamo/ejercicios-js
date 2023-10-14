eeb093766111ee5dedc697a26d7ad1dc64f033f1
Francisco B <fbiasuso@gmail.com>
2023-10-14 00:35:13 -0300

    Ejercicio 26 completado

---
+++ ejercicio-26/ejercicio-26.js
@@ -0,0 +1,16 @@
+
+
+const cantidad = Math.ceil(Math.random()*100)
+const numeros = [];
+
+for (let index = 0; index < cantidad; index++) {
+    numeros.push(Math.ceil(Math.random()*100)) 
+}
+
+let suma = 0;
+for (let index = 0; index < numeros.length; index++) {
+    suma =  suma + numeros[index];
+    
+}
+console.log('%c Ejercicio 26:', 'background: #000; color: #fff')
+console.log('La sumatoria de los ',numeros.length,'números es: ',suma);
