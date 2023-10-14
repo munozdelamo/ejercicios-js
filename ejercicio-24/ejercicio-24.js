ae7793bf15b2dfc49ee7c8ef03f57dc8aefb6eec
Francisco B <fbiasuso@gmail.com>
2023-10-14 03:04:49 -0300

    Ejercicio 24 completado

---
+++ ejercicio-24/ejercicio-24.js
@@ -0,0 +1,41 @@
+console.log('%c Ejercicio 24:', 'background: #000; color: #fff')
+
+const calculadora = ()=>{
+    const num1 = prompt("Ingresá el primer número: ");
+    const operador = prompt("Ingresa un operador (  +,   -,   *,   /  ) ");
+    const num2 = prompt("Ingresá el segundo número: ");
+    
+    let operacion = 0;
+    const numero1 = Number(num1);
+    const numero2 = Number(num2);
+    if (typeof(numero1) !== "number" || Number.isNaN(numero1) || numero1 === null ) { 
+        alert("El primer dato ingresado no es un número"); 
+        return confirm("¿Desea realizar otra operación?");
+    }
+    if (typeof(numero2) !== "number" || Number.isNaN(numero2) || numero2 === null) {  
+        alert("El segundo dato ingresado no es un número"); 
+        return confirm("¿Desea realizar otra operación?");
+    }
+    
+    operador == "+" ? (operacion = numero1 + numero2 ) 
+    : operador == "-" ? (operacion = numero1 - numero2) 
+    : operador == "*" ? (operacion = numero1 * numero2)
+    : operador == "/" ? (operacion = numero1 / numero2)   
+    : (alert("No te hagas el vivo. Seleccioná un operador válido."),operador = false);
+    
+    if(!operador){
+        return confirm("¿Desea realizar otra operación?");    
+    }
+    
+    alert("La operación es : "+ numero1 +" " + operador + " " + numero2 + " = " + operacion)
+    console.log("La operación es : ",numero1," ",operador," ",numero2," = ",operacion);
+
+    return confirm("¿Desea realizar otra operación?");
+}
+
+while(calculadora());
+
+
+
+
+
