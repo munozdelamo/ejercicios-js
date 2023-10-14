14f23e7c88a1a7a70196a43061a268ae704cb00b
Francisco B <fbiasuso@gmail.com>
2023-10-14 15:09:31 -0300

    Ejercicio 23 completado

---
+++ ejercicio-23/ejercicio-23.js
@@ -0,0 +1,41 @@
+console.log('%c Ejercicio 23:', 'background: #000; color: #fff');
+ 
+ const minimo = Number(prompt("Elija un valor mínimo para el rango:"));
+ console.log("El Mínimo del rango es: ",minimo)
+ let maximo = Number(prompt("Elija un valor máximo para el rango (de al menos\n 5 números de diferencia con respecto al mínimo):"));
+ 
+ (maximo - minimo < 5 ) ? ((maximo = minimo + 5),console.log("El Máximo del rango es: ",maximo))
+ : console.log("El Máximo del rango es: ",maximo)
+ 
+ 
+
+
+ const cantidad = Math.ceil(Math.random()*100)
+ console.log("La cantidad de valores desconocidos es: ",cantidad)
+const numeros = []
+
+for (let index = 0; index < cantidad; index++) {
+   
+    /* numeroNuevo = Math.floor(Math.random()*maximo);
+    (maximo - numeroNuevo > 5 ) && numeros.push(numeroNuevo); */
+    let aleatorio = Math.floor(Math.random()*maximo);
+     numeros.push(aleatorio);
+    
+}
+console.log("Los valores desconocidos son: ",numeros) 
+for (let index = 0; index < numeros.length; index++) {
+     (numeros[index] >= minimo && numeros[index] <= maximo) 
+     ? (numeros[index] % 2 == 0) && console.log("El Número: ",numeros[index]," es PAR.")
+     : (numeros[index] % 2 == 1) && console.log("El Número: ",numeros[index]," es IMPAR.")
+    
+}
+
+
+{/* <p> Se le solicita al usuario que ingrese los extremos de un rango numérico y una cantidad de valores desconocidos</p>
+    <p> Realice un programa para informar si cada número está dentro del rango.</p>
+    <p> Si está en rango, informar si el número es par.</p>
+    <p> Si no está dentro del rango, informar si es número es impar.</p>
+    <p> Tenga en cuenta que el rango debe tener una diferencia mínima de 5 números enteros</p>
+ */}
+
+
