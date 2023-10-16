718bfc4f9f68b70e2477cde997157ab5403535f7
Francisco B <fbiasuso@gmail.com>
2023-10-16 03:37:08 -0300

    Ejercicio 10 Completado

---
+++ ejercicio-10/ejercicio-10.js
@@ -0,0 +1,38 @@
+/*
+	Se le solicita al usuario que ingrese el diámetro de un círculo. Realice un programa 
+    para calcular e informar el perímetro y el área del círculo. Se considerará para este 
+    ejercicio que el valor de PI será 3.1416.
+*/
+
+console.log('%c Ejercicio 10:', 'background: #000; color: #fff')
+
+const calcularSueldoTotal = ()=>{
+			
+	const diametro = Number(prompt("Escriba el diámetro del círculo: "))
+	if(!validarNumero(diametro)){
+		console.log("Imposible calcular el perimetro y el área del círculo sin el diámetro.")
+		return false
+	}
+	console.log('El diámtro del círculo es : ',diametro)
+	
+	const perímetro = 3.1416 * diametro;
+	const radio = diametro / 2;
+	console.log("El perimetro del círculo es: ",perímetro)
+	const area = 3.1416 * (radio * radio);
+	console.log("El Área del circulo es: ",area)
+}
+
+
+const validarNumero = (numero)=>{
+	
+	if (typeof(numero) !== "number" || Number.isNaN(numero) || numero === null || numero === "") { 
+	 
+		console.log('ERROR: El dato ingresado no es un número válido.')
+		
+		return false
+	}
+		
+	return	true
+}
+
+calcularSueldoTotal();
