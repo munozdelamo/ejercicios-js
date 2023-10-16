c722a50692920a2c8af8add91b00f4b90cbaa6e1
Francisco B <fbiasuso@gmail.com>
2023-10-15 21:44:21 -0300

    Ejercicio 16 Completado

---
+++ ejercicio-16/ejercicio-16.js
@@ -0,0 +1,35 @@
+ /*  
+     Se le solicita al usuario que ingrese un número. Realice un programa para informar 
+     si el número es múltiplo de 3
+ */
+
+console.log('%c Ejercicio 16:', 'background: #000; color: #fff');
+ 
+ const num1 = Number(prompt("Escriba un número:"));
+ console.log("El número ingresado es: ",num1)
+ 
+ const multiplos = (num1)=>{
+     
+     esUnNumero(num1)
+     esMultiplo(num1) 
+     
+ }
+
+const esUnNumero = (numero)=>{
+     
+     if (typeof(numero) !== "number" || Number.isNaN(numero) || numero === null ) { 
+        alert("El dato ingresado no es un número válido"); 
+        return confirm("¿Desea realizar otra operación?");
+    }
+}
+
+const esMultiplo = (numero)=>{
+     (numero % 3 == 0) 
+     ? console.log("El  número ingresado es multiplo de 3")
+     : console.log("El  número ingresado NO es multiplo de 3")
+     
+   
+}
+
+   
+multiplos(num1)
